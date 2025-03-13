dc352d11589af87d90d1fa4dea21dcd9dc28bf13
Amartya <kamartya58@gmail.com>
2025-03-14 01:01:41 +0530

    Fixed bug of navigation to main activity when user relogins

--- app/src/main/java/com/example/worka1/ui/account/AccountFragment.kt
+++ app/src/main/java/com/example/worka1/ui/account/AccountFragment.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -24,7 +26,6 @@ class AccountFragment : Fragment() {
     private val binding get() = _binding!!
     private lateinit var auth : FirebaseAuth
     private lateinit var googleSignInClient: GoogleSignInClient
-
     companion object {
         fun newInstance() = AccountFragment()
     }
@@ -41,35 +42,27 @@ class AccountFragment : Fragment() {
     ): View? {
         _binding = FragmentAccountBinding.inflate(inflater, container, false)
         val view = binding.root
+        auth = FirebaseAuth.getInstance()
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build()
+        googleSignInClient = GoogleSignIn.getClient(requireActivity(),gso)
+
         val editButton = binding.editprofile
         editButton.setOnClickListener {
             findNavController().navigate(R.id.navigation_edit)
         }
         val myBookingCard = binding.cardMyBookings
-        val paymentsCard = binding.cardPayments
         val helpSupportCard = binding.cardHelpSupport
-        val signOutBtn = binding.signOutBtn
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build()
-        googleSignInClient = GoogleSignIn.getClient(requireActivity(),gso)
-        signOutBtn.setOnClickListener {
-            signOut() // sign out the user
-        }
+
         myBookingCard.setOnClickListener {
             findNavController().navigate(R.id.navigation_mybookings)
         }
 
-        paymentsCard.setOnClickListener {
-            findNavController().navigate(R.id.navigation_native_device)
-        }
 
         helpSupportCard.setOnClickListener {
             findNavController().navigate(R.id.navigation_helpandsupport)
         }
 
         val images = listOf(
-            R.drawable.ic_my_plan,
-            R.drawable.wallet,
-            R.drawable.ic_plus_membership,
             R.drawable.ic_my_rating,
             R.drawable.ic_my_location,
             R.drawable.payment_method,
@@ -77,22 +70,33 @@ class AccountFragment : Fragment() {
             R.drawable.ic_profile_24
         )
         val names = listOf(
-            "My Plans",
-            "Wallet",
-            "Plus membership",
             "My rating",
             "Manage addresses",
             "Manage payment methods",
             "Settings",
             "About WorkA1",
-
         )
 
         val adapter = AccountAdapter(images, names, findNavController())
 
         binding.accountRecyclerView.layoutManager = LinearLayoutManager(requireContext())
         binding.accountRecyclerView.adapter = adapter
+        val logout_button = view.findViewById<LinearLayout>(R.id.logout_button)
+        val logout_button_text = view.findViewById<TextView>(R.id.logout_button_text)
 
+        if(auth.currentUser == null){
+            logout_button_text.text = "Login"
+            logout_button.setOnClickListener {
+                val intent = Intent(requireContext(), LogInActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        else{
+            logout_button_text.text = "Logout"
+            logout_button.setOnClickListener {
+                signOut()
+            }
+        }
         return view
     }
 
@@ -100,7 +104,8 @@ class AccountFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
-// Sign out function
+
+    // Sign out function
     private fun signOut(){
         auth = FirebaseAuth.getInstance()
         auth.signOut()
@@ -111,4 +116,5 @@ class AccountFragment : Fragment() {
             }
 
     }
+
 }
